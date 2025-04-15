60c8a71a5e55008eb163b8e3ccc37cf859279335
Antonio Herazo <antonioherazo7@hotmail.com>
2025-04-15 17:52:21 -0500

    se separa widget de la grafica

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -1,13 +1,24 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+// -----------------------------------------------------------------------------
+// Sección: Imports
+// -----------------------------------------------------------------------------
+
+import 'dart:convert'; // Importa la biblioteca 'dart:convert' para trabajar con la codificación y decodificación de datos, como JSON.
+import 'package:http/http.dart'
+    as http; // Importa la biblioteca 'http' del paquete 'http' y le asigna el alias 'http' para realizar solicitudes HTTP.
+
+// -----------------------------------------------------------------------------
+// Sección: Clase Consumo (Modelo de Datos)
+// -----------------------------------------------------------------------------
 
 class Consumo {
-  final String fecha;
-  final String hora;
-  final double amperios;
-  final double consumoKwh;
+  // Clase modelo para representar un dato de consumo.
+  final String fecha; // Fecha del registro de consumo.
+  final String hora; // Hora del registro de consumo.
+  final double amperios; // Valor de los amperios en el registro.
+  final double consumoKwh; // Valor del consumo en kWh en el registro.
 
   Consumo({
+    // Constructor de la clase Consumo.
     required this.fecha,
     required this.hora,
     required this.amperios,
@@ -15,31 +26,70 @@ class Consumo {
   });
 
   factory Consumo.fromJson(Map<String, dynamic> json) {
+    // Método factory para crear una instancia de Consumo desde un mapa JSON.
     return Consumo(
-      fecha: json['fecha'],
-      hora: json['hora'],
-      amperios: json['amperios'],
-      consumoKwh: json['consumo_kwh'],
+      fecha: json['fecha'], // Obtiene el valor de la clave 'fecha' del JSON.
+      hora: json['hora'], // Obtiene el valor de la clave 'hora' del JSON.
+      amperios:
+          (json['amperios'] as num)
+              .toDouble(), // Obtiene el valor de la clave 'amperios' del JSON, lo castea a 'num' (para manejar tanto int como double) y luego lo convierte a 'double'.
+      consumoKwh:
+          (json['consumo_kwh'] as num)
+              .toDouble(), // Obtiene el valor de la clave 'consumo_kwh' del JSON, lo castea a 'num' y luego lo convierte a 'double'.
     );
   }
 }
 
+// -----------------------------------------------------------------------------
+// Sección: Clase ApiService (Servicio de API)
+// -----------------------------------------------------------------------------
+
 class ApiService {
-  static const String baseUrl = "http://54.207.201.160:5000";
+  // Clase que contiene métodos estáticos para interactuar con la API.
+  static const String baseUrl =
+      'http://54.207.201.160:5000'; // Define una constante estática para la URL base de la API.
+
+  // ---------------------------------------------------------------------------
+  // Sección: Método estático obtenerConsumo()
+  // ---------------------------------------------------------------------------
 
   static Future<List<Consumo>> obtenerConsumo({
-    int offset = 0,
-    int limit = 50,
+    // Método estático asíncrono para obtener una lista de objetos Consumo desde la API.
+    int offset =
+        0, // Parámetro opcional para la paginación, indica el punto de inicio de los datos a solicitar (por defecto es 0).
+    int limit =
+        500, // Parámetro opcional para la paginación, indica la cantidad de datos a solicitar (por defecto es 50).
   }) async {
-    final response = await http.get(
-      Uri.parse('$baseUrl/consumo?offset=$offset&limit=$limit'),
-    );
+    // Construye la URL completa para la solicitud GET, incluyendo los parámetros de offset y limit.
+    final url = Uri.parse('$baseUrl/consumo?offset=$offset&limit=$limit');
 
-    if (response.statusCode == 200) {
-      final List<dynamic> data = json.decode(response.body);
-      return data.map((item) => Consumo.fromJson(item)).toList();
-    } else {
-      throw Exception('Error al obtener los datos');
+    try {
+      // Bloque try-catch para manejar posibles errores durante la llamada a la API.
+      final response = await http.get(
+        url,
+      ); // Realiza una solicitud HTTP GET a la URL construida. La palabra clave 'await' indica que esta operación es asíncrona.
+
+      if (response.statusCode == 200) {
+        // Verifica si la respuesta de la API fue exitosa (código de estado 200 OK).
+        final List<dynamic> data = json.decode(
+          response.body,
+        ); // Decodifica el cuerpo de la respuesta (que se espera que sea JSON) en una lista dinámica.
+        return data
+            .map(
+              (json) => Consumo.fromJson(json),
+            ) // Itera sobre cada elemento (que se espera que sea un mapa JSON) en la lista 'data' y lo convierte a un objeto Consumo utilizando el método factory 'fromJson'.
+            .toList(); // 🔁 Convierte el resultado del 'map' (un Iterable) a una lista de objetos Consumo.
+      } else {
+        // Si la respuesta de la API no fue exitosa, lanza una excepción con el código de estado y la razón del error.
+        throw Exception(
+          'Error ${response.statusCode}: ${response.reasonPhrase}',
+        );
+      }
+    } catch (e) {
+      // Captura cualquier excepción que ocurra durante la llamada a la API (por ejemplo, problemas de red).
+      throw Exception(
+        'Excepción al obtener datos: $e',
+      ); // Lanza una nueva excepción con un mensaje descriptivo del error.
     }
   }
 }
--- lib/screens/graph_screen.dart
+++ lib/screens/graph_screen.dart
@@ -1,7 +1,8 @@
+// Paso 1: Estructura básica para scroll infinito con gráfico y filtros de hora
+
 import 'package:flutter/material.dart';
-//import 'package:intl/intl.dart';
-import '/api_service.dart';
 import 'package:fl_chart/fl_chart.dart';
+import '/api_service.dart';
 
 class GraphScreen extends StatefulWidget {
   const GraphScreen({super.key});
@@ -11,25 +12,90 @@ class GraphScreen extends StatefulWidget {
 }
 
 class _GraphScreenState extends State<GraphScreen> {
-  late Future<List<Consumo>> _datosFuturos;
+  List<Consumo> _datos = [];
+  int _offset = 0;
+  final int _limit = 50;
+  bool _isLoading = false;
+  bool _hasMore = true;
+
   int _horaInicio = 0;
-  int _horaFin = 23;
+  int _horaFin = 2;
+
+  final ScrollController _scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
-    _datosFuturos = ApiService.obtenerConsumo();
+    _cargarDatos();
+
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels >=
+          _scrollController.position.maxScrollExtent - 200) {
+        _cargarDatos();
+      }
+    });
   }
 
-  //---- Funcion para dibujar un DropDown
+  Future<void> _cargarDatos() async {
+    if (_isLoading || !_hasMore) return;
+    setState(() => _isLoading = true);
+
+    try {
+      final nuevos = await ApiService.obtenerConsumo(
+        offset: _offset,
+        limit: _limit,
+      );
+
+      print("➡️ Datos recibidos (${nuevos.length}):");
+      for (var d in nuevos) {
+        print("🕒 ${d.hora} | ⚡ ${d.amperios} A");
+      }
+
+      if (nuevos.isEmpty) {
+        _hasMore = false;
+      } else {
+        setState(() {
+          _datos.addAll(nuevos);
+          _offset += _limit;
+        });
+      }
+    } catch (e) {
+      print("Error al cargar datos: $e");
+    } finally {
+      setState(() => _isLoading = false);
+    }
+  }
+
   List<DropdownMenuItem<int>> _generarOpcionesHoras() {
-    return List.generate(24, (index) {
-      return DropdownMenuItem(value: index, child: Text('$index:00'));
-    });
+    return List.generate(
+      24,
+      (index) => DropdownMenuItem(value: index, child: Text('$index:00')),
+    );
+  }
+
+  List<FlSpot> _generarSpots(List<Consumo> datosFiltrados) {
+    return List.generate(
+      datosFiltrados.length,
+      (index) => FlSpot(index.toDouble(), datosFiltrados[index].amperios),
+    );
+  }
+
+  double _intervaloEtiquetas(List<Consumo> datosFiltrados) {
+    final total = datosFiltrados.length;
+    if (total <= 10) return 1;
+    return (total / 10).floorToDouble();
   }
 
   @override
   Widget build(BuildContext context) {
+    final datosFiltrados =
+        _datos.where((d) {
+          final hora = int.parse(d.hora.split(':')[0]);
+          return hora >= _horaInicio && hora <= _horaFin;
+        }).toList();
+
+    print("📊 Total de datos acumulados: \${_datos.length}");
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Gráfico de Consumo'),
@@ -37,145 +103,103 @@ class _GraphScreenState extends State<GraphScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: FutureBuilder<List<Consumo>>(
-          future: _datosFuturos,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-            } else {
-              final datos = snapshot.data!;
-
-              // 🟡 Filtrar datos por hora seleccionada
-              final datosFiltrados =
-                  datos.where((d) {
-                    final hora = int.parse(d.hora.split(':')[0]);
-                    return hora >= _horaInicio && hora <= _horaFin;
-                  }).toList();
-
-              // 🟣 Imprimir los datos filtrados en consola
-              print(
-                '🔍 Datos filtrados para el rango $_horaInicio:00 - $_horaFin:00',
-              );
-              for (var d in datosFiltrados) {
-                print('Hora: ${d.hora}, Amperios: ${d.amperios}');
-              }
-
-              if (datosFiltrados.isEmpty) {
-                return const Center(
-                  child: Text('No hay datos para el rango seleccionado.'),
-                );
-              }
-
-              // 🔵 Convertir datos a puntos del gráfico
-              final spots = List.generate(datosFiltrados.length, (index) {
-                return FlSpot(index.toDouble(), datosFiltrados[index].amperios);
-              });
-
-              double calcularIntervaloEtiquetas() {
-                final total = datosFiltrados.length;
-                if (total <= 10) return 1;
-                return (total / 10).floorToDouble();
-              }
-
-              return Column(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Desde: "),
+                DropdownButton<int>(
+                  value: _horaInicio,
+                  items: _generarOpcionesHoras(),
+                  onChanged: (valor) {
+                    setState(() => _horaInicio = valor!);
+                  },
+                ),
+                const SizedBox(width: 20),
+                const Text("Hasta: "),
+                DropdownButton<int>(
+                  value: _horaFin,
+                  items: _generarOpcionesHoras(),
+                  onChanged: (valor) {
+                    setState(() => _horaFin = valor!);
+                  },
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView(
+                controller: _scrollController,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text("Desde: "),
-                      DropdownButton<int>(
-                        value: _horaInicio,
-                        items: _generarOpcionesHoras(),
-                        onChanged: (valor) {
-                          setState(() {
-                            _horaInicio = valor!;
-                            _datosFuturos = ApiService.obtenerConsumo();
-                          });
-                        },
-                      ),
-                      const SizedBox(width: 20),
-                      const Text("Hasta: "),
-                      DropdownButton<int>(
-                        value: _horaFin,
-                        items: _generarOpcionesHoras(),
-                        onChanged: (valor) {
-                          setState(() {
-                            _horaFin = valor!;
-                            _datosFuturos =
-                                ApiService.obtenerConsumo(); // <-- vuelve a llamar la API
-                          });
-                        },
-                      ),
-                    ],
+                  SizedBox(
+                    height: 400,
+                    child:
+                        _datos.isEmpty && _isLoading
+                            ? const Center(child: CircularProgressIndicator())
+                            : graficaDeLinea(datosFiltrados),
                   ),
-                  const SizedBox(height: 20),
-                  Expanded(
-                    child: LineChart(
-                      LineChartData(
-                        minY: 0,
-                        maxY: 20,
-                        gridData: FlGridData(show: true),
-                        titlesData: FlTitlesData(
-                          bottomTitles: AxisTitles(
-                            sideTitles: SideTitles(
-                              showTitles: true,
-                              reservedSize: 60,
-                              interval: calcularIntervaloEtiquetas(),
-                              getTitlesWidget: (value, meta) {
-                                int index = value.toInt();
-                                if (index >= 0 &&
-                                    index < datosFiltrados.length) {
-                                  final hora = datosFiltrados[index].hora;
-                                  return SideTitleWidget(
-                                    axisSide: meta.axisSide,
-                                    child: Transform.rotate(
-                                      angle: -1.5708, // -90 grados en radianes
-                                      child: Text(
-                                        hora,
-                                        style: const TextStyle(fontSize: 10),
-                                      ),
-                                    ),
-                                  );
-                                }
-                                return const SizedBox.shrink();
-                              },
-                            ),
-                          ),
-                          leftTitles: AxisTitles(
-                            sideTitles: SideTitles(
-                              showTitles: true,
-                              reservedSize: 40,
-                              interval: 5,
-                            ),
-                          ),
-                          rightTitles: AxisTitles(
-                            sideTitles: SideTitles(showTitles: false),
-                          ),
-                          topTitles: AxisTitles(
-                            sideTitles: SideTitles(showTitles: false),
-                          ),
-                        ),
-                        lineBarsData: [
-                          LineChartBarData(
-                            spots: spots,
-                            isCurved: false,
-                            color: Colors.blue,
-                            barWidth: 2,
-                            dotData: FlDotData(
-                              show: false,
-                            ), // 🔵 Sin circulitos
-                          ),
-                        ],
-                      ),
+                  if (_isLoading)
+                    const Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Center(child: CircularProgressIndicator()),
                     ),
-                  ),
                 ],
-              );
-            }
-          },
+              ),
+            ),
+          ],
         ),
+      ),
+    );
+  }
+
+  LineChart graficaDeLinea(List<Consumo> datosFiltrados) {
+    return LineChart(
+      LineChartData(
+        minY: 0,
+        maxY: 20,
+        gridData: FlGridData(show: true),
+        titlesData: FlTitlesData(
+          bottomTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              reservedSize: 60,
+              interval: _intervaloEtiquetas(datosFiltrados),
+              getTitlesWidget: (value, meta) {
+                int index = value.toInt();
+                if (index >= 0 && index < datosFiltrados.length) {
+                  final hora = datosFiltrados[index].hora;
+                  return SideTitleWidget(
+                    axisSide: meta.axisSide,
+                    child: Transform.rotate(
+                      angle: -1.5708,
+                      child: Text(hora, style: const TextStyle(fontSize: 10)),
+                    ),
+                  );
+                }
+                return const SizedBox.shrink();
+              },
+            ),
+          ),
+          leftTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              reservedSize: 40,
+              interval: 5,
+            ),
+          ),
+          rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+          topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+        ),
+        lineBarsData: [
+          LineChartBarData(
+            spots: _generarSpots(datosFiltrados),
+            isCurved: false,
+            color: Colors.blue,
+            barWidth: 2,
+            dotData: FlDotData(show: false),
+          ),
+        ],
       ),
     );
   }
